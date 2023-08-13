a1652a4ac39e7732da0af4e35db146fb93da9cda
ThilinaSatoru <satorugamage1@gmail.com>
2023-08-13 23:52:55 +0530

    bootstrap fixes

--- views/public/family_register.php
+++ views/public/family_register.php
@@ -1,86 +1,99 @@
 <?php
-if(isset($_POST["register"]))
-{
-    $ID=$_POST["familyID"];
+if (isset($_POST["register"])) {
+    $ID = $_POST["familyID"];
 
-   if ($ret > 0)//check return
-   {
+    if ($ret > 0) //check return
+    {
 
-    header("location:BabyVaccneCard.php");
-   }
-   else
-   {
-    echo '<script>alert("Please Try Again Shortly.....")</script>';
-    header("location:NurseRegistration.php");
-   }
+        header("location:BabyVaccneCard.php");
+    } else {
+        echo '<script>alert("Please Try Again Shortly.....")</script>';
+        header("location:NurseRegistration.php");
+    }
 }
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/MOH-office/resources/css/register.css">
     <title>Public user register</title>
+    <style>
+        .login-box {
+            width: 35rem;
+            margin-inline: 30%;
+            padding: 1em;
+            border-radius: 1em;
+            box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+        }
+    </style>
 </head>
+
 <body>
-   
+    <?php include('../../views/templates/header.php'); ?>
+
     <div class="container">
-        <h1>  Registration</h1>
-         <form method="POST" action="#">
-            <div class="user-details">
-                
+        <h1> Registration</h1>
+        <div class="card login-box">
+            <form method="POST" action="family_register.php">
 
-            <div class="input-box">
-                 <span class="details">Family ID</span>
-                <input type="text" name="familyID" placeholder="Enter Family ID" required>
-                 </div>
-
-
-                <div class="input-box">
-                 <span class="details"> Name</span>
-                <input type="text" name="name" placeholder="Enter your name" required>
-                 </div>
-    
-                <div class="input-box">
-                    <span class="details">Email</span>
-                    <input type="text" name="email" placeholder="Enter your name" required>
-                </div>  
-                
-                <div class="input-box">
-                    <span class="details">Contact num:</span>
-                    <input type="text" name="contact" placeholder="Enter your name" required>
+                <div class="mb-3">
+                    <label class="form-label">NIC :</label>
+                    <input class="form-control" type="text" name="nic" placeholder="Enter your NIC" required>
                 </div>
-    
-                    <div class="input-box">
-                        <span class="details">Username</span>
-                        <input type="text" name="us" placeholder="Enter your name" required>
-                    </div>
-    
-                    <div class="input-box">
-                        <span class="details">Password</span>
-                        <input type="password" name="pw" placeholder="password" required>
-                    </div>
-    
-                    <div class="input-box">
-                        <span class="details">Confirm Password</span>
-                        <input type="password" name="conformpw" placeholder="Confirm password" required>
-                    </div>
-    
-                    <div class="button">
-                        <input type="submit" name="register" value="register">
-                    </div>
-        </div> 
 
-         </form>
-   
-            
-       
+                <div class="mb-3">
+                    <label class="form-label">Full name :</label>
+                    <input class="form-control" type="text" name="name" placeholder="Enter your name" required>
+                </div>
 
+                <div class="mb-3">
+                    <label class="form-label">Email :</label>
+                    <input class="form-control" type="email" name="email" placeholder="Enter your email" required>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Contact :</label>
+                    <input class="form-control" type="text" name="contact" placeholder="Enter your contact number" required>
+                </div>
+
+                <hr>
+
+                <div class="mb-3">
+                    <label class="form-label">Username :</label>
+                    <input class="form-control" type="text" name="username" placeholder="Enter your username" required>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Password :</label>
+                    <input class="form-control" type="password" name="password" placeholder="Enter your password" required>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Confirm Password :</label>
+                    <input class="form-control" type="password" name="re_password" placeholder="Confirm password" required>
+                </div>
+
+
+                <div class="card-body text-center">
+                    <button type="submit" class="btn btn-primary" name="new_family">Register</button>
+                    <a class="icon-link icon-link-hover link-success link-underline-success link-underline-opacity-25" href="family_login.php">
+                        Alredy have an account?
+                        <svg class="bi" aria-hidden="true">
+                            <use xlink:href="#arrow-right"></use>
+                        </svg>
+                    </a>
+                </div>
+
+            </form>
+        </div>
     </div>
-    
+
+    <?php include('../../views/templates/footer.php'); ?>
 </body>
+
 </html>
--- views/public/staff_login.php
+++ views/public/staff_login.php
@@ -1,164 +1,102 @@
+<?php
+session_start(); // Starting Session
+include_once("../../../MOH-office/core/repository/accountRepository.php");
+$ACCOUNT_REPO = new AccountRepository();
+
+if (isset($_POST["btnLOGIN"])) {
+
+    $account = new Account(
+        null,
+        null,
+        $_POST["username"],
+        $_POST["password"],
+        $_POST["user_type"]
+    );
+
+    $result = $ACCOUNT_REPO->findByUsernameAndPassword($account);
+    if ($result) {
+        if ($result->getType() === $_POST["user_type"]) {
+            // Store user type in the session
+            $_SESSION['username'] = $result->getUsername();
+            $_SESSION['user_type'] = $result->getType();
+
+            if ($result->getType() === "DOCTOR") {
+                // Redirecting To Doctor Main Page
+                header("location: ../staff/doctor/");
+                exit();
+            } elseif ($result->getType() === "NURSE") {
+                // Redirecting To Nurse Main Page
+                header("location: ../staff/nurse/");
+                exit(); // Stop further execution
+            }
+        } else {
+            echo '<script>alert("Account doesnt exists from given Type.")</script>';
+        }
+    } else {
+        echo '<script>alert("Invalid! Please Try Again.")</script>';
+    }
+}
+?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <!-- <link rel="stylesheet" href="/MOH-office/resources/css/register.css"> -->
-    <!-- Design by foolishdeveloper.com -->
-    <title>Doctor and Nurse Login</title>
-    <style media="screen">
-        *,
-        *:before,
-        *:after {
-            padding: 0;
-            margin: 0;
-            box-sizing: border-box;
-        }
-
-        body {
-            background-color: #07003b;
-        }
-
-        .background {
-            width: 430px;
-            height: 520px;
-            position: absolute;
-            transform: translate(-50%, -50%);
-            left: 50%;
-            top: 50%;
-        }
-
-        .background .shape {
-            height: 200px;
-            width: 200px;
-            position: absolute;
-            border-radius: 50%;
-        }
-
-        .shape:last-child {
-            background: linear-gradient(
-                to right,
-                #ff512f,
-                #ff9500
-            );
-            right: -30px;
-            bottom: -80px;
-        }
-
-        form {
-            height: 520px;
-            width: 400px;
-            background-color: rgba(255, 255, 255, 0.13);
-            position: absolute;
-            transform: translate(-50%, -50%);
-            top: 50%;
-            left: 50%;
-            border-radius: 10px;
-            backdrop-filter: blur(10px);
-            border: 2px solid rgba(255, 255, 255, 0.1);
-            box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
-            padding: 50px 35px;
-        }
-
-        form * {
-            font-family: 'Poppins', sans-serif;
-            color: #ffffff;
-            letter-spacing: 0.5px;
-            outline: none;
-            border: none;
-        }
-
-        form h3 {
-            font-size: 32px;
-            font-weight: 500;
-            line-height: 42px;
-            text-align: center;
-            color: #4d1717;
-        }
-
-        label {
-            display: block;
-            margin-top: 30px;
-            font-size: 16px;
-            font-weight: 500;
-        }
-
-        input {
-            display: block;
-            height: 50px;
-            width: 100%;
-            background-color: rgba(255, 255, 255, 0.07);
-            border-radius: 3px;
-            padding: 0 10px;
-            margin-top: 8px;
-            font-size: 14px;
-            font-weight: 300;
-        }
-
-        ::placeholder {
-            color: #e5e5e5;
-        }
-
-        button {
-            margin-top: 50px;
-            width: 100%;
-            background-color: #42729c;
-            color: #11d9d9;
-            padding: 15px 0;
-            font-size: 18px;
-            font-weight: 600;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background: #07003b;
-        }
-
-        select {
-            display: block;
-            height: 50px;
-            width: 100%;
-            background-color: rgba(255, 255, 255, 0.07);
-            border-radius: 3px;
-            padding: 0 10px;
-            margin-top: 8px;
-            font-size: 14px;
-            font-weight: 300;
+    <style>
+        .login-box {
+            width: 18rem;
+            margin-inline: 40%;
+            padding: 1em;
+            border-radius: 1em;
+            box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
         }
     </style>
+    <title>Doctor and Nurse Login</title>
 </head>
 
 <body>
+    <?php include('../../views/templates/header.php'); ?>
 
 
-    <div class="background">
-        <div class="button">
-            <a href="/MOH-office/index.php">Home</a>
-        </div>
-
-    </div>
-    <form method="POST" action="login.php">
+    <div class="container">
         <h3>Staff Login</h3>
-        <label for="user_type">Staff Type</label>
-        <select id="user_type" name="user_type">
-            <option value="doctor">Doctor</option>
-            <option value="nurse">Nurse</option>
-        </select>
+        <div class="card login-box">
+            <form method="POST" action="staff_login.php">
+                <div class="mb-3">
+                    <label for="user_type">Staff Type</label>
+                    <select class="form-select" id="user_type" name="user_type">
+                        <option value="DOCTOR">Doctor</option>
+                        <option value="NURSE">Nurse</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="username" class="form-label">User Name</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                    <div id="emailHelp" class="form-text"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" required>
+                </div>
 
-        <label for="username">Username</label>
-        <input type="text" placeholder="Username" name="username" required>
+                <div class="card-body text-center">
+                    <button type="submit" class="btn btn-primary" name="btnLOGIN">Login</button>
+                    <a class="icon-link icon-link-hover link-success link-underline-success link-underline-opacity-25" href="staff_register.php">
+                        don't have an account?
+                        <svg class="bi" aria-hidden="true">
+                            <use xlink:href="#arrow-right"></use>
+                        </svg>
+                    </a>
+                </div>
 
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" name="password" required>
+            </form>
+        </div>
+    </div>
 
-        <button type="submit" name="btnLOGIN">Log In</button>
-        <br>
-        <br>
-        <a href="staff_register.php">register here</a>
-        
-        
-    </form>
+    <?php include('../../views/templates/footer.php'); ?>
 </body>
 
 </html>
